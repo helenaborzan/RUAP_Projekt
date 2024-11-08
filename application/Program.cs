e1e55b83fb5471fd4bfb853790308c524b6d8158
helenaborzan <helena.borzan@gmail.com>
2024-11-08 15:49:36 +0100

    changed message

--- application/Program.cs
+++ application/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Helena Borzan");
+            Console.WriteLine("Pozdrav iz aplikacije od Helena Borzan, broj indeksa 47474747");
         }
     }
 }
