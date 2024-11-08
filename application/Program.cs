35fd921a379682bf0426121c8386808849c16c5e
helenaborzan <helena.borzan@gmail.com>
2024-11-08 15:48:26 +0100

    created app

---
+++ application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Helena Borzan");
+        }
+    }
+}
